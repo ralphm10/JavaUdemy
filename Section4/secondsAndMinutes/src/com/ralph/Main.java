d18625a4d776c732d5ed0d5e32991a30fe785b8a
Ralph Mallett <rmallett12@gmail.com>
2021-01-18 14:27:13 +0000

    Section 4: add constant message to seconds & mins

--- Section4/secondsAndMinutes/src/com/ralph/Main.java
+++ Section4/secondsAndMinutes/src/com/ralph/Main.java
@@ -2,18 +2,20 @@ package com.ralph;
 
 public class Main {
 
+    private static final String INVALID_VALUE_MESSAGE = "Invalid value";
+
     public static void main(String[] args) {
         String test = getDurationString(65, 20);
         System.out.println(test);
 
-        String secondTest = getDurationString(3665);
+        String secondTest = getDurationString(3945);
         System.out.println(secondTest);
     }
 
 
     public static String getDurationString(int minutes, int seconds) {
         if (minutes < 0 || seconds < 0 || seconds > 59) {
-            return "Invalid value";
+            return INVALID_VALUE_MESSAGE;
         }
 
         int hours = minutes / 60;
@@ -24,7 +26,7 @@ public class Main {
 
     public static String getDurationString(int seconds) {
         if (seconds < 0) {
-            return "Invalid value";
+            return INVALID_VALUE_MESSAGE;
         }
 
         int minutes = seconds / 60;
